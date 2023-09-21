a555148411307b65720ab0788949c18b31bbd3df
Angelina <50414739+angelinalavoryk@users.noreply.github.com>
2023-09-21 20:46:09 +0200

    Home page

---
+++ src/components/Feature/Feature.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Feature({ icon, title, description }) {
+  return (
+    <div className="feature-item">
+      <img src={icon} alt="Feature Icon" className="feature-icon" />
+      <h3 className="feature-item-title">{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default Feature;
